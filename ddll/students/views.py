a3a6dadc967116d9fb821929f1fb8ade8e6b68b0
LOEE-PYTHON <744236737@qq.com>
2020-09-28 14:33:04 +0800

    20200928 14:32 jinke

--- ddll/students/views.py
+++ ddll/students/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
-from students.models import StudentInfo, MoneyInfo, StudentClassInfo, ClassDetailInfo
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+from students.models import StudentInfo, MoneyInfo, StudentClassInfo, ClassDetailInfo, UserInfo
 
 
 def index(request):
@@ -7,9 +7,34 @@ def index(request):
 
 
 def login(request):
-    return render(request, 'students/login.html')
+    u_name = request.COOKIES.get("u_name", '')
+    context = {'u_name':u_name, 'error_name':0, 'error_pwd':0}
+    return render(request, 'students/login.html',context)
 
 
+def login_handle(request):
+    u_name = request.POST.get('username')
+    u_pwd = request.POST.get('pwd')
+    jizhu = request.POST.get('jizhu',0)
+    user = UserInfo.objects.filter(u_name = u_name)
+    print(u_name)
+    if len(user)==1:
+        print('用户名验证成功！')
+        if user[0].u_pwd == u_pwd:
+            red = HttpResponseRedirect('/students/student_into/')
+            if jizhu != 0:
+                red.set_cookie('u_name', u_name)
+            else:
+                red.set_cookie('u_name', '', max_age=-1)
+            request.session['u_id'] = user[0].u_id
+            request.session['u_name'] = user[0].u_name
+            return red
+        else:
+            context ={'u_name':u_name,'error_name':0,'error_pwd':1}
+            return render(request,'students/login.html',context)
+    else:
+        context = {'u_name':u_name,'error_name':1,'error_pwd':0}
+        return render(request, 'students/login.html', context)
 def student_into(request):
     return render(request, 'students/student_into.html')
 
@@ -47,30 +72,36 @@ def student_list_handle(request):
         for i in student_info:
             s_id = i.id
             over_course_num = MoneyInfo.objects.filter(m_id_id=s_id)
+            use_course_num = ClassDetailInfo.objects.filter(s_id_id=s_id).count()
             num = 0
             for m_info in over_course_num:
                 num += m_info.m_regular_time
-                num_dic[i.s_id] = num
+                num1 = num - use_course_num
+                num_dic[i.s_id] = num1
 
     elif q_method == "2":
         student_info = StudentInfo.objects.filter(s_name__contains=query_info)
         for i in student_info:
             s_id = i.id
             over_course_num = MoneyInfo.objects.filter(m_id_id=s_id)
+            use_course_num = ClassDetailInfo.objects.filter(s_id_id=s_id).count()
             num = 0
             for m_info in over_course_num:
                 num += m_info.m_regular_time
-                num_dic[i.s_id] = num
+                num1 = num - use_course_num
+                num_dic[i.s_id] = num1
 
     elif q_method == "3":
         student_info = StudentInfo.objects.filter(s_into__contains=query_info)
         for i in student_info:
             s_id = i.id
             over_course_num = MoneyInfo.objects.filter(m_id_id=s_id)
+            use_course_num = ClassDetailInfo.objects.filter(s_id_id=s_id).count()
             num = 0
             for m_info in over_course_num:
                 num += m_info.m_regular_time
-                num_dic[i.s_id] = num
+                num1 = num - use_course_num
+                num_dic[i.s_id] = num1
     else:
         return HttpResponse("没有找到！")
 
@@ -159,3 +190,44 @@ def student_add_handle(request):
         MoneyInfo.objects.create(s_info.id, m_money, m_pay_date, m_usually, m_special, m_discount, m_note)
     context = {'s_name':s_name,"s_gender":s_gender,"s_phone":s_phone,"s_birthday":s_birthday,"s_into":s_into,"s_note":s_note}
     return render(request,'students/student_add.html', context)
+
+
+def student_info_alter(request):
+    if request.method == 'POST':
+        post = request.POST
+        s_id = post.get('s_id')
+        print(s_id)
+        s_info = StudentInfo.objects.get(s_id=s_id)
+        # s_info = StudentInfo.objects.get(s_id='DDLL0001')
+        s_info.s_name = post.get('s_name')
+        s_info.s_gender = post.get('s_gender')
+        s_info.s_birthday = post.get("s_birthday")
+        s_info.s_phone = post.get("s_phone")
+        s_info.s_status = post.get("s_status")
+        s_info.s_into = post.get("s_into")
+        s_info.s_note = post.get("s_note")
+        s_info.save()
+        print('保存成功！')
+        # return HttpResponse('保存成功！')
+    else:
+        request_info = request.GET
+        s_id = request_info.get('s_id')
+        s_info = StudentInfo.objects.get(s_id=s_id)
+        id = request_info.get('id')
+        s_name = s_info.s_name
+        s_gender = s_info.s_gender
+        if s_gender:
+            s_gender = '男'
+        else:
+            s_gender = "女"
+        s_birthday = s_info.s_birthday
+        s_phone = s_info.s_phone
+        s_status = s_info.s_status
+        if s_status:
+            s_status = '在读'
+        else:
+            s_status = '停课'
+        s_into = s_info.s_into
+        s_note = s_info.s_note
+        context = {'s_name':s_name,'s_id':s_id,'s_gender':s_gender,'s_birthday':s_birthday,'s_phone':s_phone,'s_status':s_status,'s_into':s_into,'s_note':s_note}
+        return render(request,'students/student_info_alter.html/',context)
